75ae0420174eeeea3f0b9c6a85f83b613c75adb9
Joris Klaasse Bos <joris.klaasse.bos@student.uva>
2023-06-07 14:31:56 +0200

    Implement simple player with input in C++ (README will follow)

---
+++ godot/src/README.md
@@ -0,0 +1,3 @@
+
+
+The Godot header files are in `PSE/godot/godot-cpp/gen/include/godot_cpp/`.
--- godot/src/player.hpp
+++ godot/src/player.hpp
@@ -1,22 +1,40 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/input.hpp>
 #include <godot_cpp/classes/sprite2d.hpp>
+#include <godot_cpp/classes/area2d.hpp>
+#include <godot_cpp/classes/collision_shape2d.hpp>
+
+#include <map>
+
+union InputValue {
+	int magnitude;
+	bool active;
+};
+
+enum InputAxis { x_input };
 
 namespace godot {
 
-	class Player : public Sprite2D {
-		GDCLASS(Player, Sprite2D)
+	class Player : public Area2D {
+		GDCLASS(Player, Area2D)
+		Sprite2D *_sprite;
+		CollisionShape2D *_collision_shape;
+		Input *_player_input;
 	private:
-		double time_passed;
+		auto get_input() -> std::map<InputAxis, InputValue>;
 	protected:
-		static void _bind_methods();
+		static auto _bind_methods() -> void;
 	public:
+		real_t speed;
 		Player();
 		~Player();
-		void _process(double delta);
+		auto _ready() -> void;
+		auto _process(double delta) -> void;
 	};
 
 }
 
-#endif
+#endif  // PLAYER_H
