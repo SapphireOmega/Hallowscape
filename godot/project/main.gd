4334a3ce70a3cb5e44e006a614b1506f53ccae58
andrea <andreali2000@icloud.com>
2023-06-12 14:47:30 +0200

    player movement via telefoon

--- godot/project/main.gd
+++ godot/project/main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var runOnStart = true
 @export var PORT = 8080
 
 var server = TCPServer.new()
@@ -10,27 +11,29 @@ var MAX_PLAYERS = 2
 var n_players = 0
 
 func _ready():
-	server.listen(PORT)
-	print(server.is_listening())
-	var PCRThread = Thread.new()
-	PCRThread.start(processConnectionRequest)
+	if runOnStart:
+		server.listen(PORT)
+		print(server.is_listening())
+		var PCRThread = Thread.new()
+		PCRThread.start(processConnectionRequest)
+		PCRThread.wait_to_finish()
 
 func processConnectionRequest():
 	while true:
 		if server.is_connection_available() and n_players <= MAX_PLAYERS:
 			var tcp = server.take_connection()
 			print("connected")
-#
+			
 			mutex.lock()
 			n_players += 1
 			mutex.unlock()
-
+			
 			print("Client " + str(n_players) + " connected")
 
 			var SCThread = Thread.new()
 			SCThread.start(serveClient.bind(tcp, n_players))
 			threads.append(SCThread)
-#
+	
 	for thread in threads:
 		thread.wait_to_finish()
 
@@ -43,17 +46,11 @@ func serveClient(tcp, clientID):
 		if bytes > 0:
 			var data = tcp.get_partial_data(bytes)
 			var action = data[1].get_string_from_utf8()
-			print("action :",action)
-			var command = JSON.parse_string(action)
-			print("after parse :",command)
-			if command:
-				if command["type"] == "pressed":
-					Input.action_press(command["action"])
-				else:
-					Input.action_release(command["action"])
+			print(action)
+			Input.action_press(action)
 
 	tcp.disconnect_from_host()
-
+	
 	mutex.lock()
 	n_players -= 1
 	mutex.unlock()
