f49fc71c0afd71143c4c384ec66c6e38740dd03d
Joris Klaasse Bos <joris.klaasse.bos@student.uva>
2023-06-15 13:38:49 +0200

    Make barrel affected by gravity

--- godot/project/Barrel/barrel.gd
+++ godot/project/Barrel/barrel.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
+@export var gravity = 1100
+
 func slide(delta: float, speed: float):
 	self.position.x += delta * speed
+	move_and_slide()
+
+func _physics_process(delta) -> void:
+	self.velocity.y += gravity * delta
 	move_and_slide()
 
 # Called when the node enters the scene tree for the first time.
