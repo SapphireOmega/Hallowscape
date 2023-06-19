5185147d552e32d40b8b2dfd9224182b7b07f955
kieran van Gelder <kieran.van.gelder@student.uva.nl>
2023-06-19 13:53:14 +0200

    makes church door go back to jorislevel

--- godot/project/Church/Area2D.gd
+++ godot/project/Church/Area2D.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 
 func _on_body_entered(body: CharacterBody2D):
-	StageManager.changeStage(StageManager.TOWN, 1960, 250)
+	StageManager.changeStage(StageManager.JORISLEVEL, 1960, 220)
 	
 
 
