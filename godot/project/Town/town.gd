7a2268f0ca704e7a54929e7b603e7e9d6d5d217a
kieran van Gelder <kieran.van.gelder@student.uva.nl>
2023-06-20 10:33:17 +0200

    added scripts to stage main nodes to store some data in

--- godot/project/Town/town.gd
+++ godot/project/Town/town.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
+const CAMLIMITS = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MusicGallery.set_music_intensity(2)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
