564862523848bd46e7c10f9b55ec903f2d74883f
kieran van Gelder <kieran.van.gelder@student.uva.nl>
2023-06-22 13:00:33 +0200

    splashscreen stuff

--- godot/project/splash_screen/splash_screen_manager.gd
+++ godot/project/splash_screen/splash_screen_manager.gd
@@ -9,15 +9,13 @@ var _splash_screens = []
 
 
 func _ready() -> void:
-
 	set_process_input(false)
 
 	for splash_screen in _splash_screen_container.get_children():
 		splash_screen.hide()
 		_splash_screens.push_back(splash_screen)
-
+	MusicGallery.play_track_by_name("pengel")
 	await get_tree().create_timer(_initial_delay).timeout
-
 	_start_splash_screen()
 
 	set_process_input(true)
@@ -29,7 +27,8 @@ func _input(_event: InputEvent) -> void:
 
 func _start_splash_screen() -> void:
 	if _splash_screens.size() == 0:
-		$"/root".get_children()
+		StageManager.changeStage(StageManager.MAINMENU)
+		
 	else:
 		var splash_screen = _splash_screens.pop_front()
 		splash_screen.start()
