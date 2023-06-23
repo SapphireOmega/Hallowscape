e72a5a3c3dac4a6e0a273a7fc9cabe1858c0016b
kieran van Gelder <kieran.van.gelder@student.uva.nl>
2023-06-23 12:09:40 +0200

    loads of imports idk where they came from, also main menu work

--- godot/project/Menus/main_menu.gd
+++ godot/project/Menus/main_menu.gd
@@ -9,8 +9,10 @@ func _ready():
 	MusicGallery.play_track_by_name("pengel")
 
 func _on_credits_button_up():
-	$Menu.hide()
+	$Menu/VBoxContainer.hide()
+	$title_background/Title.hide()
 	$Credits.show()
+	$Credits/Panel/Main_menu.grab_focus()
 
 
 #quits the game
@@ -22,9 +24,15 @@ func _on_quit_button_up():
 
 func _on_start_button_up():
 	MusicGallery.stop_track_by_name("pengel")
-	#add below whatever the start scene is
-	
 	StageManager.changeStage(StageManager.JORISLEVEL, 50, 312)
 	var server = preload("res://TestServerClients/Server.tscn").instantiate()
 	$"/root".add_child(server)
-#	$"/root/".move_child(server,0)
+
+
+
+
+func _on_main_menu_button_up():
+	$Credits.hide()
+	$Menu/VBoxContainer.show()
+	$title_background/Title.show()
+	$Menu/VBoxContainer/Credits.grab_focus()
