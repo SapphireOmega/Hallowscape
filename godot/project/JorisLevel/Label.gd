d49640995701e6cfcc38d0c251223af942caab31
Joris Klaasse Bos <joris.klaasse.bos@student.uva>
2023-06-26 13:02:10 +0200

    Fade tutorial labels in and make lasers not collide with Area2Ds

---
+++ godot/project/JorisLevel/Label.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	create_tween().tween_property($Label, "modulate:a", 1, 1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("body_entered", _on_body_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
