a509f1a86f7cc9e8b1366d82a98b5036efae089a
Joris Klaasse Bos <joris.klaasse.bos@student.uva>
2023-06-27 14:51:28 +0200

    Fix Label.gd and add header comment

--- godot/project/JorisLevel/Label.gd
+++ godot/project/JorisLevel/Label.gd
@@ -1,11 +1,13 @@
+### Make a message appear when a player enters an area.
+
 extends Area2D
 
-func _on_body_entered(_body) -> void:
+func _on_body_entered(body) -> void:
+	if !body.has_method("die"): return
 	for child in get_children():
 		create_tween().tween_property(child, "modulate:a", 1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	connect("body_entered", _on_body_entered)
-
-
+	
