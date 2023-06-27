d3761f4caad29e12f4431000a70f63676808f495
Sven Roozendaal <sven.roozendaal@student.uva.nl>
2023-06-27 10:08:30 +0200

    Camera boundries added

---
+++ godot/project/World/KillBorder.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	monitoring = true
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.has_method("push_barrels"):
+		StageManager.kill_players()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
