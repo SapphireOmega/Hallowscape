b1460ce0fae4175fffcd8b6547fb3e4fef62d17d
kieran van Gelder <kieran.van.gelder@student.uva.nl>
2023-06-30 13:11:56 +0200

    made server on optional

--- godot/project/Menus/main_menu.gd
+++ godot/project/Menus/main_menu.gd
@@ -6,6 +6,8 @@ const CAMLIMITS = {
 	"left" :0,
 	"right" :640,
 	}
+	
+var server_on = false
 
 func _ready():
 	$Menu/VBoxContainer/Start.grab_focus() #selects the start button so you can start with enter
@@ -25,11 +27,13 @@ func _on_quit_button_up():
 
 
 
+
 func _on_start_button_up():
 	MusicGallery.stop_track_by_name("pengel")
 	StageManager.changeStage(StageManager.JORISLEVEL, 50, 312)
-	var server = preload("res://TestServerClients/Server.tscn").instantiate()
-	$"/root".add_child(server)
+	if server_on:
+		var server = preload("res://TestServerClients/Server.tscn").instantiate()
+		$"/root".add_child(server)
 
 
 
@@ -39,3 +43,7 @@ func _on_main_menu_button_up():
 	$Menu/VBoxContainer.show()
 	$title_background/Title.show()
 	$Menu/VBoxContainer/Credits.grab_focus()
+
+
+func _on_check_button_toggled(button_pressed):
+	server_on = button_pressed
